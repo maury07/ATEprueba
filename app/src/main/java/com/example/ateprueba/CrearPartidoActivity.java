f2ee106514cd9f99acae2422cb0a385d5bcf0b27
maury07 <maury.-n@live.com.ar>
2019-09-21 00:14:46 -0300

    Agregue sharedPreferences a la activity CrearPartido para que mande los datos a mostrar en el FragmentPartido ( ## ARREGLAR DESPUES ## )

--- app/src/main/java/com/example/ateprueba/CrearPartidoActivity.java
+++ app/src/main/java/com/example/ateprueba/CrearPartidoActivity.java
@@ -2,7 +2,9 @@ package com.example.ateprueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,15 +21,39 @@ public class CrearPartidoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_partido);
+        titulo = (EditText)findViewById(R.id.etNombrePartido);
+        cancha = (EditText)findViewById(R.id.etNombreCancha);
+        fecha = (EditText)findViewById(R.id.etFechaPartido);
+        hora = (EditText)findViewById(R.id.etHoraPartido);
 
         crearPartido = (Button)findViewById(R.id.btnCrearPartido);
         crearPartido.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Se ha creado un nuevo partido", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), titulo.getText().toString(), Toast.LENGTH_SHORT).show();
+                SharedPreferences preferences = getSharedPreferences
+                        ("partidos", Context.MODE_PRIVATE);
+
+                String nombrePartido = titulo.getText().toString();
+                String nombreCancha = cancha.getText().toString();
+                String fechaPart = fecha.getText().toString();
+                String horaPart = hora.getText().toString();
+                SharedPreferences.Editor editor=preferences.edit();
+                editor.putString("nombre",nombrePartido);
+                editor.putString("cancha",nombreCancha);
+                editor.putString("fecha", fechaPart);
+                editor.putString("hora",horaPart);
+                editor.putInt("visible",1);
+
+                editor.commit();
                 Intent i = new Intent(CrearPartidoActivity.this, TabbedActivity.class);
+                //crearPartido();
                 startActivity(i);
             }
         });
+    }
+
+    private void crearPartido() {
+
     }
 }
--- app/src/main/java/com/example/ateprueba/fragments/PartidoFragment.java
+++ app/src/main/java/com/example/ateprueba/fragments/PartidoFragment.java
@@ -1,5 +1,7 @@
 package com.example.ateprueba.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ateprueba.Models.PartidoModelo;
@@ -26,17 +29,31 @@ public class PartidoFragment extends Fragment {
     private FloatingActionButton fab;
     Button postularse;
     List<PartidoModelo> lista;
+    String nombre, cancha, fecha, hora;
+    TextView nohaypartidos;
+    int visible;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_partido, container, false);
-
+        nohaypartidos = (TextView)view.findViewById(R.id.tvNoHayPartidos);
         recyclerViewPartido = (RecyclerView)view.findViewById(R.id.recyclerPartido);
         recyclerViewPartido.setLayoutManager(new LinearLayoutManager(getContext()));
+        cargarPref();
 
-        lista = obtenerPartidos();
+
+
+        lista = obtenerPartidos(nombre, cancha, hora);
+        if(visible == 0)
+        {
+            nohaypartidos.setVisibility(View.VISIBLE);
+        }
+        else{
+            nohaypartidos.setVisibility(View.INVISIBLE);
+            recyclerViewPartido.setVisibility(View.VISIBLE);
+        }
 
         adaptadorPartido = new RecyclerViewAdaptadorPartido(lista);
         recyclerViewPartido.setAdapter(adaptadorPartido);
@@ -44,20 +61,32 @@ public class PartidoFragment extends Fragment {
         return view;
     }
 
-    public List<PartidoModelo> obtenerPartidos(){
+    public List<PartidoModelo> obtenerPartidos(String nombre, String cancha, String hora){
         List<PartidoModelo> partido = new ArrayList<>();
         //Partido(String titulo, String nombrePerfil, String tipoPartido, int imgPerfil, int imgCalendario, int imgUbicacion,
         // int imgHora, int imgDisciplina, int imgFaltan, String ubicacion, String hora, String disciplina)
-        partido.add(new PartidoModelo("Partido mixto", "Damian Villarreal", "Partido Mixto", R.drawable.pelado1,
-                R.drawable.ventiocho, R.drawable.ic_location, R.drawable.hour, R.drawable.futbol2, R.drawable.number5, "Futbol City 1", "18:00 hs", "Futbol"));
-        partido.add(new PartidoModelo("Partido importante", "Mauricio Nuñez", "Futbol Masculino", R.drawable.maury1,
+        partido.add(new PartidoModelo(nombre, "Damian Villarreal", "Partido Mixto", R.drawable.pelado1,
+                R.drawable.ventiocho, R.drawable.ic_location, R.drawable.hour, R.drawable.futbol2, R.drawable.number5, cancha, hora, "Futbol"));
+        /*partido.add(new PartidoModelo("Partido importante", "Mauricio Nuñez", "Futbol Masculino", R.drawable.maury1,
                 R.drawable.veintinueve, R.drawable.ic_location, R.drawable.hour, R.drawable.futbol2, R.drawable.number5,  "Futbol City 2", "21:00 hs", "Futbol"));
         partido.add(new PartidoModelo("Partido tranqui", "Matias colorado", "Futbol Masculino", R.drawable.mati1,
-                R.drawable.veintinueve, R.drawable.ic_location, R.drawable.hour, R.drawable.futbol2, R.drawable.number5,  "Los Olivos", "23:00 hs", "Futbol"));
+                R.drawable.veintinueve, R.drawable.ic_location, R.drawable.hour, R.drawable.futbol2, R.drawable.number5,  "Los Olivos", "23:00 hs", "Futbol"));*/
 
 
         return partido;
     }
+
+    public void cargarPref(){
+        SharedPreferences preferences = this.getActivity().getSharedPreferences
+                ("partidos", Context.MODE_PRIVATE);
+        nombre = preferences.getString("nombre", "No hay nombre");
+        cancha = preferences.getString("cancha", "No hay cancha");
+        fecha = preferences.getString("fecha", "No hay fecha");
+        hora = preferences.getString("hora", "no hay hora");
+        visible = preferences.getInt("visible",0);
+
+    }
+
 
 
 }
